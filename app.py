180a486e0b92878c5bd4a5f16fbdc22eb0cc9704
EC2 Default User <ec2-user@ip-172-31-33-103.ec2.internal>
2021-02-23 22:10:24 +0000

    Added player array and modified how moves are captured in Board.js, modified app.py

--- app.py
+++ app.py
@@ -46,17 +46,14 @@ def on_disconnect():
 # 'choice' is a custom event name that we just decided
 @socketio.on('choice')
 def on_choice(data): # data is whatever arg you pass in your emit call on client
-    #print(str(data))
-    # This emits the 'choice' event from the server to all clients except for
-    # the client that emmitted the event that triggered this function
     socketio.emit('choice', data, broadcast=True, include_self=False)
-
 
 @socketio.on('player_joined') #When player active, name is added to player board
 def on_player_joined(data):
     print(str(data))
     socketio.emit('player_joined', data, broadcast=True, include_self=False)
     
+
 if __name__ == '__main__':
     socketio.run(
         app,
--- src/Board.js
+++ src/Board.js
@@ -8,7 +8,8 @@ import { socket } from './App.js';
 export function Board (){
     const inputRef = useRef(null);
     var winner = null;
-    var two_player = {X: -1, O: -1};
+    const [two_player, setPlayer] = useState({X: -1, O: -1});
+    const [turns, changeTurn] = useState({turn: 0});
     const [board, setBoard] = useState([
                                         {id: 0, symbol: ""},
                                         {id: 1, symbol: ""},
@@ -20,7 +21,7 @@ export function Board (){
                                         {id: 7, symbol: ""},
                                         {id: 8, symbol: ""},
                                         ]);
-                                        
+                                      
     function check(old_board, squareId, symbol){
         const lines = [
                         [0, 1, 2],//top horizontal
@@ -40,7 +41,7 @@ export function Board (){
             var b = lines[i][1];
             var c = lines[i][2];
             if(board[a].symbol == board[b].symbol && board[a].symbol == board[c].symbol){
-                if(board[0].symbol != "") //So that it won't count three consecutive blanks as a game over
+                if(board[a].symbol != "" || board[b].symbol != "" || board[c].symbol != "") //So that it won't count three consecutive blanks as a game over
                     return true;
             }
         }
@@ -48,6 +49,7 @@ export function Board (){
     }
     
     function onClickSymbol(squareId, symbol){
+        var data_board = board;
         if (inputRef != null){
             console.log(squareId);
             var symb = "";
@@ -56,6 +58,7 @@ export function Board (){
             }else if(symbol == "O" || symbol == ""){
                 symb = "X";
             }
+            var i = 0;
             setBoard(board.map((square) => square.id == squareId ? {...square, symbol: symb} : square));
             var _check_ = check(board, squareId, symb);
             console.log(_check_);
@@ -63,7 +66,8 @@ export function Board (){
                 winner = true;
                 console.log("Game over");
             }//Add check for if board is full
-            socket.emit('choice', {squareId: squareId, symb: symb, winner:winner }); // emits event
+            data_board.map((square) => square.id == squareId ? square.symbol = symb : square);
+            socket.emit('choice', {board: data_board, winner: winner}); // emits event, sending entire board and winner
             console.log({squareId: squareId, symb: symb, winner: winner })
             if(winner == true){
                 console.log("You win!");
@@ -73,30 +77,52 @@ export function Board (){
  
   useEffect(() => {
     socket.on('connect', (data) => {
-        if(data != undefined){
+        if(data != undefined && data[0].length % 2 == 0 && two_player.O == -1){
             console.log(data[0]);
             console.log(data[1]);
-            if(data[0].includes(data[1]) && two_player.X == -1){
-                two_player.X = data[1];
-            }else if(data[0].includes(data[1]) && two_player.X != -1){
-                two_player.O = data[1];
+            if(data[0][1] == data[1] && two_player.X == -1){
+                setPlayer(prevPlayer => {
+                    return {X : data[1], O : -1};
+                });
+                //two_player.X = data[0][1];
+            }else if(data[0][3] == data[1] && two_player.X != -1 && data[1] > two_player.X){
+                setPlayer(prevPlayer => {
+                    return { X: data[0][1], O : data[0][3]};
+                });
             }
-            console.log(two_player);
         }
-        //console.log(data[1]);
+        console.log(two_player);
         //Find out how to do session variable
     });
     
     socket.on('choice', (data) => { //responds when 'choice' is emitted
       //console.log('Choice event received!');
       console.log(data);
-      setBoard(board.map((square) => square.id == data.squareId ? {...square, symbol: data.symb} : square));
+      var new_board = data.board;
+      console.log(new_board[0].symbol);
+      var i = 0;
+      setBoard(board.map((square) => {
+          if(square.id == new_board[i].id){
+              return {...square, symbol: new_board[i++].symbol};
+          }else{
+              i++;
+          }
+      }));
+      /*
+      changeTurn(prevTurn => {
+        if(turns.turn == 0){
+            return { turn : 1 };
+        }else{
+            return { turn : 0 };
+        }
+      });
+      */
       if(data.winner == true){
           console.log("You lost");
       }
     });
   }, [board]); //put board so that it will save the changes and not reset the board after choice
-
+  console.log(two_player);
     return (
         <div className="board">
                 {board.map((square, index) =>
